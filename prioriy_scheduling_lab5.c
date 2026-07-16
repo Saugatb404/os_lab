1978c4f38fea9ef2485016ad9c7aea462a1a1904
Saugat_Bhattarai <saugatb11001@gmail.com>
2026-07-16 19:37:30 +0545

    lab5 is added

--- prioriy_scheduling_lab5.c
+++ prioriy_scheduling_lab5.c
@@ -1,56 +1,135 @@
-#include<stdio.h>
-#include<conio.h>
-struct process{
-int pid; int bt; int wt; int tt; int prior;
-}
-p[10],temp;
-int main(){
-int i,j,n,totwt,tottt,arg1,arg2;
-clrscr();
-printf("enter the number of process");
-scanf("%d",&n);
-for(i=1;i<=n;i++){
-p[i].pid=i;
-printf("enter the burst time");
-scanf("%d",&p[i].bt);
-printf("\n enter the priority");
-scanf("%d",&p[i].prior);
-}
-for(i=1;i<n;i++){
-for(j=i+1;j<=n;j++){
-if(p[i].prior>p[j].prior){
-temp.pid=p[i].pid;
-p[i].pid=p[j].pid;
-p[j].pid=temp.pid;
-temp.bt=p[i].bt;
-p[i].bt=p[j].bt;
-p[j].bt=temp.bt;
-temp.prior=p[i].prior;
-p[i].prior=p[j].prior;
-p[j].prior=temp.prior;
-}
-}
-}
-p[i].wt=0;
-p[1].tt=p[1].bt+p[1].wt;
-i=2;
-while(i<=n){
-p[i].wt=p[i-1].bt+p[i-1].wt;
-p[i].tt=p[i].bt+p[i].wt;
-i++;
-}
-i=1;
-totwt=tottt=0;
-printf("\n process to \t bt \t wt \t tt");
-while(i<=n){
-printf("\n%d\t %d\t %d\t %d\t",p[i].pid,p[i].bt,p[i].wt,p[i].tt);
-totwt=p[i].wt+totwt;
-tottt=p[i].tt+tottt;
-i++;
-}
-arg1=totwt/n;
-arg2=tottt/n;
-printf("\n arg1=%d \t arg2=%d\t",arg1,arg2);
-getch();
-return 0;
+#include <stdio.h>
+
+/*
+    Structure representing one process.
+*/
+struct Process
+{
+    int pid;             // Process ID
+    int burstTime;       // CPU Burst Time
+    int priority;        // Smaller number = Higher Priority
+    int waitingTime;     // Waiting Time
+    int turnaroundTime;  // Turnaround Time
+};
+
+int main()
+{
+    int n;
+
+    printf("=========================================\n");
+    printf(" Non-Preemptive Priority Scheduling\n");
+    printf("=========================================\n\n");
+
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    struct Process process[n];
+    struct Process temp;
+
+    //----------------------------------------
+    // Input Process Information
+    //----------------------------------------
+
+    for (int i = 0; i < n; i++)
+    {
+        process[i].pid = i + 1;
+
+        printf("\nProcess %d\n", process[i].pid);
+
+        printf("Enter Burst Time : ");
+        scanf("%d", &process[i].burstTime);
+
+        printf("Enter Priority   : ");
+        scanf("%d", &process[i].priority);
+    }
+
+    //----------------------------------------
+    // Sort by Priority
+    // Smaller Priority Number = Higher Priority
+    //----------------------------------------
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (process[i].priority > process[j].priority)
+            {
+                temp = process[i];
+                process[i] = process[j];
+                process[j] = temp;
+            }
+        }
+    }
+
+    //----------------------------------------
+    // Calculate Waiting Time
+    //----------------------------------------
+
+    process[0].waitingTime = 0;
+
+    for (int i = 1; i < n; i++)
+    {
+        process[i].waitingTime =
+            process[i - 1].waitingTime +
+            process[i - 1].burstTime;
+    }
+
+    //----------------------------------------
+    // Calculate Turnaround Time
+    //----------------------------------------
+
+    for (int i = 0; i < n; i++)
+    {
+        process[i].turnaroundTime =
+            process[i].waitingTime +
+            process[i].burstTime;
+    }
+
+    //----------------------------------------
+    // Calculate Totals
+    //----------------------------------------
+
+    int totalWaitingTime = 0;
+    int totalTurnaroundTime = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        totalWaitingTime += process[i].waitingTime;
+        totalTurnaroundTime += process[i].turnaroundTime;
+    }
+
+    //----------------------------------------
+    // Calculate Averages
+    //----------------------------------------
+
+    double averageWaitingTime =
+        (double)totalWaitingTime / n;
+
+    double averageTurnaroundTime =
+        (double)totalTurnaroundTime / n;
+
+    //----------------------------------------
+    // Display Result
+    //----------------------------------------
+
+    printf("\n-----------------------------------------------------------------\n");
+    printf("PID\tPriority\tBurst\tWaiting\tTurnaround\n");
+    printf("-----------------------------------------------------------------\n");
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d\t%d\t\t%d\t%d\t%d\n",
+               process[i].pid,
+               process[i].priority,
+               process[i].burstTime,
+               process[i].waitingTime,
+               process[i].turnaroundTime);
+    }
+
+    printf("-----------------------------------------------------------------\n");
+
+    printf("Average Waiting Time    : %.2f\n", averageWaitingTime);
+    printf("Average Turnaround Time : %.2f\n", averageTurnaroundTime);
+
+    return 0;
 }
